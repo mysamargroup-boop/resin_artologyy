7bc30927ec62c88276926fdfd14184404e46bb79
My Projects <my7projects@gmail.com>
2026-03-05 18:32:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "resinartologyy-2-7926039-e6036",
+  "appId": "1:349064763294:web:7df8b74f7b27d7259e688f",
+  "apiKey": "AIzaSyAeP0HftlxHgc-CkI-gEIp8hjCsZvZFoxk",
+  "authDomain": "resinartologyy-2-7926039-e6036.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "349064763294"
+};
